405db206162827cc11b3d652149d041d7dd3e578
carro <cqz@u.northwestern.edu>
2023-01-26 21:01:17 -0600

    account for no tags

--- menu.js
+++ menu.js
@@ -1,6 +1,5 @@
 tagList = document.getElementById("highlight-add-tags");
-a = 0
-b = 1
+
 function expandMenu() {
 	// make the menu and tag list wider
 	parent = document.getElementById("highlight-add-modal");
@@ -14,21 +13,19 @@ function expandMenu() {
 
 	// make list items flex items and break words normally
 	var tags = tagList.getElementsByTagName("li");
-	a = tags.length;
-	for (var i = 0; i < tags.length-1; i++) {
+	for (var i = 0; i < tags.length; i++) {
 		tags[i].style.flex = '1 0 25%';
 		tags[i].style.margin = '0 2% 0 2%';
-		tags[i].getElementsByTagName("label")[0].style.wordBreak = 'normal';
+		label = tags[i].getElementsByTagName("label")
+		if (label.length) {
+		label[0].style.wordBreak = 'normal';
+		}
 	}
 }
 expandMenu();
 
 tagList.addEventListener('DOMNodeInserted', function(e) {
 	// the way taguette updates the list is by removing everything from the old list and readding every tag so unfortunately we have to do this once for each tag haha
-	
-	console.log("peekaboo")
-	if (b % a == 0) {
-		expandMenu();
-	}	
-	b++;
+	expandMenu();
+	console.log("did styling")
 });
